1b7040fed91fc663271717218cc67e1206255831
Kadir Özbek <kadirozbek232@gmail.com>
2024-10-14 21:28:20 +0300

    Add project files.

---
+++ BLL2/DAL2/Students.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+
+namespace BLL2.DAL2
+{
+    internal class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double GPA { get; set; }
+    }
+}
